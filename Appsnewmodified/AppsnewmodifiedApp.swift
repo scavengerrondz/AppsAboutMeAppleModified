fe44bc195f49c8b69fd2f3e4529c224a892a807d
Zaim Zawawi <mzaimzawawi@gmail.com>
2023-08-24 09:42:59 +0800

    Initial Commit

---
+++ Appsnewmodified/AppsnewmodifiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppsnewmodifiedApp.swift
+//  Appsnewmodified
+//
+//  Created by Mohd Zaim Bin Abdullah Zawawi on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppsnewmodifiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appsnewmodified/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Appsnewmodified
+//
+//  Created by Mohd Zaim Bin Abdullah Zawawi on 24/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
